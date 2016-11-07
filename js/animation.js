0f5b213f58c0af182a70a58d1a1ff7224e26e2ab
Faishal Azka <azukineru@gmail.com>
2016-11-07 20:51:31 +0700

    first commit

---
+++ js/animation.js
@@ -0,0 +1,17 @@
+
+/*
+/* Pop-up Modal 
+var $popup = $("[data-popup]"),
+    pHov   = false;
+$popup.hover(function(){ pHov^=1; });
+$(document).mousedown(function(){
+  if(!pHov) $popup.slideUp(0); 
+});
+
+$("[data-pop]").click(function(){
+  var n = $(this).data('pop');
+  var $popup = n ? $("[data-popup='"+n+"']") : $(this).closest("[data-popup]");
+  $popup.slideToggle(0);
+});
+
+*/
